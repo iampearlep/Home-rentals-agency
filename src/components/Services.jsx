e28e92adadf648bfbe31b3044c679c553ef05426
iampearlep <damilolaprincess4@gmail.com>
2023-01-26 06:04:06 +0100

    update components

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -10,15 +10,15 @@ import Vector6 from "../assets/Vector (9).svg";
 const Services = () => {
   return (
     <div className="container mx-auto mt-20">
-      <h2 className="text-lg md:text-2xl font-bold md:ml-10 capitalize text-center md:text-left mb-4 leading-relaxed md:mb-0">
+      <h2 className="w-11/12 md:w-full mx-auto text-lg md:text-2xl font-bold md:ml-10 capitalize text-center md:text-left mb-4 leading-relaxed md:mb-0">
         Minimum living cost takes care of everything{" "}
-        <hr className="border-orange-600 -mt-8 ml-10 md:ml-0 md:-mt-0 w-52 border-t-0 border-b-2" />
+        <hr className="border-orange-600 hidden md:block -mt-8 ml-10 md:ml-0 md:-mt-0 w-52 border-t-0 border-b-2" />
       </h2>
       <div className="flex flex-col md:flex-row">
         <div>
           <img src={Cost} alt="" />
         </div>
-        <div className="grid grid-cols-2 gap-x-4 gap-y-10 md:grid-cols-3 md:py-24">
+        <div className="grid grid-cols-2 gap-x-4 gap-y-10 md:grid-cols-3 md:py-24 mb-20 md:mb-0">
           <div className="flex flex-col justify-center items-center text-center md:text-left md:justify-start md:items-start">
             <div className="w-10 h-10 flex items-center justify-center rounded-md bg-white shadow-xl mb-3">
               <img className="w-6 h-6" src={Vector1} alt="" />
