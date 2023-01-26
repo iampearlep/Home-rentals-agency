4b67dc012050496263d07a6f90f366491eaffbd3
iampearlep <damilolaprincess4@gmail.com>
2023-01-26 04:46:16 +0100

    update components

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import testimonialImg from '../assets/testimonialImg (1).svg';
 import contentCard from '../assets/content-card-item-8 (1).svg'
+import Slider from './Slider';
 
 const Testimonials = () => {
 return (
   <div className='mt-32 bg-lightOrange'>
     <div className='flex flex-col md:flex-row gap-8 mx-auto'>
-      <div className='mx-10 pt-16 md:w-2/3 md:pl-28 md:pt-28'>
+      {/* <div className='mx-10 pt-16 md:w-2/3 md:pl-28 md:pt-28'>
         <p className='text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam interdum nisl et nunc facilisis, a commodo eros mollis. Nunc vel pellentesque est. Curabitur at odio sit amet libero vulputate efficitur ac nec justo. Nulla vitae mauris quam. Nulla quam massa, faucibus id pretium ac, mattis eu velit. Donec sed risus a lacus fringilla finibus.</p>
         <div className='flex flex-row gap-4 mt-8'>
           <img className='w-10 h-10' src={testimonialImg} alt="" />
@@ -16,7 +16,8 @@ return (
           </div>
         </div>
         <h2 className='text-gray text-6xl'><span className='text-orange-600'>.</span>..</h2>
-      </div>
+      </div> */}
+<Slider />
       <div className='md:w-1/2'>
   <img  src={contentCard} alt="" />
       </div>
