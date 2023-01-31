3ac84f601c0d0976f1d47288c1399f51966fd400
iampearlep <damilolaprincess4@gmail.com>
2023-02-01 00:03:24 +0100

    update components

--- src/components/navbar/Header.jsx
+++ src/components/navbar/Header.jsx
@@ -32,13 +32,10 @@ const Header = () => {
         </div>
         <hr className="mx-auto relative z-10 mt-3 border-gray-300 w-10/12" />
       </div>
-      <div className="mx-auto container md:mt-10 mt-20">
+      <div className="mx-auto relative container md:mt-10 mt-20 z-20">
       <div className="md:w-11/12 flex flex-col items-center justify-center md:flex-row md:space-x-24">
         <div className="w-11/12 md:mt-20 md:w-7/12">
           <h1
-            data-aos="fade-right"
-            data-aos-offset="200"
-            data-aos-easing="ease-in-out"
             className="text-3xl text-white md:text-5xl font-bold text-center leading-tight tracking-wide md:w-11/12 md:ml-24 md:text-left capitalize"
           >
             The most affortable place to stay in the san franciso bay area
@@ -46,11 +43,6 @@ const Header = () => {
         </div>
         <div className="flex flex-col justify-center items-center mt-5">
           <div
-            data-aos="zoom-in-up"
-            data-aos-offset="220"
-            data-aos-delay="300"
-            data-aos-easing="ease-in-out"
-            className="mapouter"
           >
             <div className="gmap_canvas">
               <iframe
